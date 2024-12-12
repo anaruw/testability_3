5fbc9598b5a72038d6a732eb088b7234fa23d4c5
Andrey Kononenko <anaruw_13@mail.ru>
2024-12-12 13:01:06 +0300

    Classes modified

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,25 +2,10 @@ public class CreditPaymentService {
 
     public int service(int credit, int period, double procent){
 
-        int result;
-
-        if (period == 1){
-
-            period = 12;
-
-        } else if (period == 2) {
-
-            period = 24;
-
-        } else if (period == 3) {
-
-            period = 36;
-        }
-
         double monthProcent = procent / 12;
         double pow = Math.pow((1 + monthProcent), period);
 
-        result = (int) (credit * monthProcent * pow / (pow - 1));
+        int result = (int) (credit * monthProcent * pow / (pow - 1));
 
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,25 @@ public class Main {
 
         CreditPaymentService monthPayment = new CreditPaymentService();
 
-        for (  int period = 1; period < 4; period ++) {
+        int period = 21; // указывать в месяцах
+        int credit = 1_000_000;
+        double procent = 0.0999;
 
-            int credit = 1_000_000;
-            double procent = 0.0999;
+        //--------------------------------------------------------------------------- пример расчета
+        for (  int i = 1; i < 4; i ++) {
+
+            int periodExample = 12 * i;
 
             System.out.println();
-            System.out.println("Для кредита на срок " + period + " год(а)");
-            System.out.println("Ежемесячный платеж составляет: " + monthPayment.service(credit, period, procent));
+            System.out.println("Для кредита на срок " + periodExample + " месяцев(а)");
+            System.out.println("Ежемесячный платеж составляет: " + monthPayment.service(credit, periodExample, procent));
         }
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\
+
+        //-------------------------------------------------------------------------- 4ый блок вывода
+        System.out.println();
+        System.out.println("Для кредита на срок " + period + " месяцев(а)");
+        System.out.println("Ежемесячный платеж составляет: " + monthPayment.service(credit, period, procent));
+        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\
     }
 }
